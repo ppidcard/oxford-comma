57da0c69c31af84e25058ae9a2d28d85d435e20a
ppidcard <tvxqlikei@live.cn>
2021-06-26 11:55:18 +1000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
-
+    if array.length > 1 && array.length < 3
+    res = []
+    i = 0
+    while i < array.length - 1
+        res << array[i] 
+        i += 1
+    end
+    res2 = res.join(', ')
+    res2 << " and #{array.last}"
+    res2
+    elsif array.length > 2
+        res = []
+        i = 0
+        while i < array.length - 1
+            res << array[i] 
+            i += 1
+        end
+        res2 = res.join(', ')
+        res2 << ", and #{array.last}"
+        res2
+    else array.join 
+    end
 end
+
+p oxford_comma(["fiddleheads", "durian"])
